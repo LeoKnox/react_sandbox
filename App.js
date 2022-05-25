5f6935d5e07172e35fcf3d89c7b0f5a08acd999d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-24 23:06:14 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import Map from "./map.js";
 
+const data = { width: 5, length: 5 };
+
 export default function App() {
   return (
     <div className="App">
       <h1>Design Dungeon</h1>
-      <Map />
+      <Map props={data} />
     </div>
   );
 }
