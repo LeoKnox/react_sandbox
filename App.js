76f2e5756d213c4ff7a9d014dcd376970760c1a5
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-24 22:49:43 -0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Map from "./map.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Design Dungeon</h1>
+      <Map />
+    </div>
+  );
+}
