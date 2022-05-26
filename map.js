b035e0b5170ee63008dc7c1b2952fe61874afbcb
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-25 21:34:31 -0700

    Update map.js

--- map.js
+++ map.js
@@ -3,6 +3,13 @@ export default function Map({ props }) {
     <div>
       <p>width:{props[0].width}</p>
       <p>height:{props[0].length}</p>
+      {(() => {
+        let x = "";
+        for (let i = 0; i < props[0].width; i++) {
+          x+="*";
+        }
+        return(x);
+      })()}
     </div>
   );
 }
