05f76874d63184bb92f314fde4dcdf7d1a643cd7
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-27 18:13:59 -0700

    Update App.js

--- characters/App.js
+++ characters/App.js
@@ -1,17 +1,6 @@
 import "./styles.css";
-import {useState} from "react";
+import Character from "./character.js";
 
 export default function App() {
-  const [player, setPlayer] = useState("fighter");
-
-  return (
-    <div className="App">
-      <>
-        <h1>Character {player}</h1>
-        <button type="button" onClick={()=>setPlayer("wizard")}>
-          wizard</button>
-        <button type="button" onClick={()=>setPlayer("fighter")}>
-          fighter</button>
-      </>
-    </div>
-  );
+  <Character character="fighter" />;
+}
