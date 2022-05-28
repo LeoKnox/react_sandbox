a1ab1d6eea52737657cbb56838d2a403a6453419
LeoKnox <LeoKnox@users.noreply.github.com>
2022-05-27 18:45:47 -0700

    Update character.js

--- characters/character.js
+++ characters/character.js
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
-export default function Character(props) {
+export default function Character() {
   const [player, setPlayer] = useState("fighter");
 
   return (
-    <>
+    <div>
       <h1>Character {player}</h1>
-    </>
+      <button
+        type="button"
+        onClick={() => setPlayer("wizard")}
+      >wizard</button>
+      <button
+        type="button"
+        onClick={() => setPlayer("fighter")}
+      >fighter</button>
+    </div>
   );
 }
