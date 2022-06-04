967cb5ce37baeca0762934aece96c992521a80f8
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-03 21:32:19 -0700

    Update App.js

--- kanji_ui/App.js
+++ kanji_ui/App.js
@@ -3,10 +3,14 @@ import "./styles.css";
 export default function App() {
   let kanjiGrades = [1,2,3,4,5];
 
+  function selectGrade() {
+    alert("grade")
+  }
+
   return (
     <div className="App">
     {kanjiGrades.map((grade, i) => (
-      <p id={i}>{grade}</p>
+      <button id={i} onClick={selectGrade}>{grade}</button>
     ))}
     </div>
   );
