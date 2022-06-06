f3cc726c66624a99689b945231ba719f840a73cb
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-05 21:34:07 -0700

    Update App.js

--- kanji_ui/App.js
+++ kanji_ui/App.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function App() {
   let kanjiGrades = [1,2,3,4,5];
-  let [grade, setGrade] = useState(1);
+  let [grade, setGrade] = useState(0);
 
   const selectGrade = () => {
     selectGrade(2, []);
@@ -15,7 +15,11 @@ export default function App() {
     {kanjiGrades.map((grade, i) => (
       <button key={i} onClick={selectGrade}>{grade}</button>
     ))}
-    <Kanjis grade="1" />
+    if (grade===0) {
+      <p>done</p>
+    } else {
+      <p>else</p>
+    }
     </div>
   );
 }
