60730aad3b31b2b7f7881032e26f6cea1e5e5958
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-06 22:51:30 -0700

    Update App.js

--- kanji_ui/App.js
+++ kanji_ui/App.js
@@ -9,6 +9,7 @@ export default function App() {
   function changeGrade(m=1) {
     switch(m) {
       case 1:
+        console.log("rture");
         return <p>true</p>;
       default:
         return <p>false</p>;
@@ -18,7 +19,7 @@ export default function App() {
   return (
     <div className="App">
     {kanjiGrades.map((grade, i) => (
-      <button key={i} onClick={changeGrade(i)}>{grade}</button>
+      <button key={i} onClick={() => changeGrade(i)}>{grade}</button>
     ))}
     <Kanjis grades={grade} />
     </div>
