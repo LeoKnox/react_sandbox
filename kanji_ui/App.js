65568e582bb2a72e6a0f51fc5e5bc3d05c3684d6
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-07 21:38:58 -0700

    Update App.js

--- kanji_ui/App.js
+++ kanji_ui/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
 import Kanjis from "./kanjis.js";
-import { useState } from "react";
+import { useState, useCallback,setRender } from "react";
 
 export default function App() {
   let kanjiGrades = [1,2,3,4,5];
   const [grade, setGrade] = useState(0);
-
+  const onChange = useCallback(() => setGrade(5),[]);
+  /*
   function changeGrade(m=1) {
     switch(m) {
       case 1:
@@ -14,14 +15,13 @@ export default function App() {
       default:
         return <p>false</p>;
     }
-  }
+  }*/
 
   return (
     <div className="App">
     {kanjiGrades.map((grade, i) => (
-      <button key={i} onClick={()=>changeGrade({grade})}>{grade}</button>
+      <button key={i} onClick={onChange}>{grade}</button>
     ))}
     <Kanjis grades={grade} />
     </div>
   );
-}
