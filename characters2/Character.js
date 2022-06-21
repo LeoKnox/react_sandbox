40369735dd9cedfb1636da8c05edbe76fca982ee
LeoKnox <LeoKnox@users.noreply.github.com>
2022-06-20 21:41:20 -0700

    Update Character.js

--- characters2/Character.js
+++ characters2/Character.js
@@ -1,5 +1,12 @@
-function Character() {
-  return <h3>Character</h3>;
+function Character(character) {
+  return (
+    <div>
+      <h3>Class: {character.class}</h3>
+      <p>Magic: {character.magic}</p>
+      <p>Armor: {character.armor}</p>
+      <p>Weapon: {Character.weapon}</p>
+    </div>
+  )
 };
 
 export default Character;
