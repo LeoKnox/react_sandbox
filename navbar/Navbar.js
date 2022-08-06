0e5f53e313def77c8dc66aa6ab0755c1ffaebf77
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-05 21:31:34 -0700

    Update Navbar.js

--- navbar/Navbar.js
+++ navbar/Navbar.js
@@ -1,3 +1,14 @@
 export default function Navbar() {
-  return <p>Navigation</p>;
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <a href="/home">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  )
 }
