f9d50b3bd63600bab09b41e0592bb84b6c1ed0b0
LeoKnox <LeoKnox@users.noreply.github.com>
2022-08-05 20:40:59 -0700

    Create App.js

---
+++ navbar/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Navbar from "./components/Navbar.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Learning some React</h1>
+    </div>
+  );
+}
