e66124099c294a58223f881af1abf959f4ee824c
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-02 21:50:20 -0700

    Update App.js

--- react_router/App.js
+++ react_router/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./styles.css";
 import Home from "./Home.js";
 import Categories from "./Categories.js";
