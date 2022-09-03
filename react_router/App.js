6f97edccb4199742d618a1d11b94aa43ab1cbdc7
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-02 21:45:33 -0700

    Update App.js

--- react_router/App.js
+++ react_router/App.js
@@ -2,14 +2,12 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./styles.css";
 import Home from "./Home.js";
 import Categories from "./Categories.js";
+import Bar from "./nav.js";
 
 export default function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/categories">Categories</Link>
-        <Link to="/"> | Home</Link>
-      </nav>
+      <Bar />
       <Routes>
         <Route path="/" element={<Home subTitle="more info to come" />} />
         <Route path="categories" element={<Categories />} />
