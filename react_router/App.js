c864a158b5279c05349f43fd55f4a87c89b5ef28
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-02 18:41:46 -0700

    Create App.js

---
+++ react_router/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Routes>
+        <Route path="/" element={<h1>routie</h1>} />
+      </Routes>
+    </div>
+  );
+}
