1e5c422dea89dfbf70463e72b9c03987332fd244
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-02 18:52:56 -0700

    Update App.js

--- react_router/App.js
+++ react_router/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "./styles.css";
+import Home from "./Home.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <Routes>
+        <Route path="/" element={<Home subTitle="more info to come" />} />
         <Route path="hello" element={<h1>routie</h1>} />
       </Routes>
     </div>
