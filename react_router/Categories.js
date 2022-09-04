c7301cac72df785978d1d47e0cbdfefb57e6d9a9
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-03 22:38:00 -0700

    Update Categories.js

--- react_router/Categories.js
+++ react_router/Categories.js
@@ -1,12 +1,12 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, Route } from "react-router-dom";
 
 export default function Categories() {
   return (
     <>
     <h2>List of Categories</h2>
     <ul>
-      <li key="1"><Link>One</Link></li>
-      <li key="2"><Link>Two</Link></li>
+      <li key="1"><Link to="1">One</Link></li>
+      <li key="2"><Link to="2">Two</Link></li>
     </ul>
     <Outlet />
     </>
