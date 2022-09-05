b1f98c6d8bb86356d77bcd5b15b5f316b260e08f
LeoKnox <LeoKnox@users.noreply.github.com>
2022-09-04 21:38:16 -0700

    Update Categories.js

--- react_router/Categories.js
+++ react_router/Categories.js
@@ -1,12 +1,20 @@
-import { Outlet, Link, Route } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 export default function Categories() {
   return (
     <>
     <h2>List of Categories</h2>
     <ul>
-      <li key="1"><Link to="1">One</Link></li>
-      <li key="2"><Link to="2">Two</Link></li>
+      <li key="1"><NavLink
+        style={({ isActive }) => ({
+          color:isActive ? "red" : "seagreen",
+        })}
+        to="1">One</NavLink></li>
+      <li key="2"><NavLink
+        style={({ isActive }) => ({
+          color:isActive ? "red" : "seagreen",
+        })}
+        to="2">Two</NavLink></li>
     </ul>
     <Outlet />
     </>
